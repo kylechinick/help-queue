bd448cc68fadbf48c1db50c0df526ae694ae5b84
Kyle Chinick <kylechinick@icloud.com>
2022-09-07 15:49:40 -0700

    Initial commit

---
+++ src/components/TicketControl.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+import TicketDetail from './TicketDetail';
+
+class TicketControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      mainTicketList: [],
+      selectedTicket: null
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  };
+
+  handleAddingNewTicketToList = newTicket => {
+    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+    this.setState({
+      mainTicketList: newMainTicketList,
+      formVisibleOnPage: false
+    });
+  };
+
+  handleChangingSelectedTicket = id => {
+    const selectedTicket = this.state.mainTicketList.filter(
+      ticket => ticket.id === id
+    )[0];
+    this.setState({ selectedTicket: selectedTicket });
+  };
+
+  render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if (this.state.selectedTicket != null) {
+      currentlyVisibleState = (
+        <TicketDetail ticket={this.state.selectedTicket} />
+      );
+      buttonText = 'Return to Ticket List';
+    } else if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = (
+        <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
+      );
+      buttonText = 'Return to Ticket List';
+    } else {
+      currentlyVisibleState = (
+        <TicketList
+          ticketList={this.state.mainTicketList}
+          onTicketSelection={this.handleChangingSelectedTicket}
+        />
+      );
+      buttonText = 'Add Ticket';
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>{' '}
+      </React.Fragment>
+    );
+  }
+}
+
+export default TicketControl;
